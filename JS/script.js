6e201c8e0f6c06775dadd6eaaa655ada7fe7ee43
q.thompson <q.thompson088@gmail.com>
2022-11-22 14:55:12 -0800

    just one event c:

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ const clickMeButton = document.querySelector(".clickMe");
 function clickHandler()
 {
     alert("Ouch")
+    clickMeButton.removeEventListener("click", clickHandler);
 }
 
-clickMeButton.addEventListener("click",clickHandler);
+clickMeButton.addEventListener("click", clickHandler /*or {once:true}*/);
