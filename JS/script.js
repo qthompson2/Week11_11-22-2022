3c4c974d893532dad45ff449c89858bec8ba3443
q.thompson <q.thompson088@gmail.com>
2022-11-22 16:23:32 -0800

    Button colors change, cannot unchange, toggle doesn't work?

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
 const clickMeButton = document.querySelector(".clickMe");
+const cartImg = document.querySelector("#cartingShop");
+const buttonContainer = document.querySelector(".buttonContainer")
 
 function turnPink()
 {
@@ -8,7 +10,7 @@ function turnPink()
 function toggleClickText()
 {
     clickMeButton.classList.toggle("isClicked");
-        if ("clickMe isClicked" === clickMeButton.classList)
+        if ("clickMe isClicked" == clickMeButton.classList)
         {
             clickMeButton.textContent = "Clicked"
         } else
@@ -17,5 +19,20 @@ function toggleClickText()
         }
 }
 
+function updateImage() 
+{
+    cartImg.setAttribute("src", "Images/263142.png");
+    cartImg.setAttribute("height", "100");
+    cartImg.setAttribute("width", "100");
+    cartImg.setAttribute("alt", "a cart of shop");
+}
+
+function changeColorWithClick(e)
+{
+    e.target.style.color = e.target.textContent
+}
+
 clickMeButton.addEventListener("click", turnPink /*or {once:true}*/);
 clickMeButton.addEventListener("click", toggleClickText);
+clickMeButton.addEventListener("click", updateImage);
+buttonContainer.addEventListener("click", changeColorWithClick);
